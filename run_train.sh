5587ad1b8a5b7e2ce036978f28b761ff1c58b303
jjjoy <204549506+jjjoy-x@users.noreply.github.com>
2025-10-02 15:09:22 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
-#SBATCH --job-name=plaszymer        # 作业名
-#SBATCH --partition=gpu3090         # gpu3090队列
-#SBATCH --qos=1gpu                  # gpu3090Qos
-#SBATCH --nodes=1                   # 节点数量
-#SBATCH --ntasks-per-node=1         # 每节点进程数
-#SBATCH --cpus-per-task=4           # 1:4 的 GPU:CPU 配比 
-#SBATCH --gres=gpu:1                # 2 块 GPU
-#SBATCH --output=logs/%j.out        # 标准输出
-#SBATCH --error=logs/%j.err         # 错误输出
-#SBATCH --mail-user=yueqing.xing22@student.xjtlu.edu.cn
-#SBATCH --mail-type=ALL
-
-cd /gpfs/work/bio/yueqingxing22/igem/Plaszymer_ml
 python train.py \
-  --train_csv /gpfs/work/bio/yueqingxing22/igem/Plaszymer_ml/data/raw/PlaszymeDB_v1_sp_train_by_ids.csv \
-  --work_dir  /gpfs/work/bio/yueqingxing22/igem/Plaszymer_ml/data/processed \
-  --model_dir /gpfs/work/bio/yueqingxing22/igem/Plaszymer_ml/model \
+  --train_csv your_train_csv_path \
+  --work_dir  your_work_dir_path \
+  --model_dir your_model_dir_path \
